d47e45eda1ba57a718cca9f42c529adf362c6c76
Etele <etele2004@gmail.com>
2026-05-15 20:47:40 +0200

    SpringBootesMavenBeallitasa

---
+++ src/main/java/org/example/zhprogkorny/ZhprogkornyApplication.java
@@ -0,0 +1,11 @@
+package org.example.zhprogkorny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhprogkornyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZhprogkornyApplication.class, args);
+    }
+}
