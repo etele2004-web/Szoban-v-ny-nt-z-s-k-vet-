354ffd0cc628a1a36388d1a0ec866e80243edd73
Etele <etele2004@gmail.com>
2026-05-16 09:45:28 +0200

    EgyketAdatHozzaadasa

---
+++ src/main/java/org/example/zhprogkorny/controller/RoomController.java
@@ -0,0 +1,27 @@
+package org.example.zhprogkorny.controller;
+
+import org.example.zhprogkorny.model.Room;
+import org.example.zhprogkorny.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rooms")
+public class RoomController {
+
+    private final RoomRepository roomRepository;
+
+    @Autowired
+    public RoomController(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    @GetMapping
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
+}
